bfa52541dd9b133b282b4faacbd5c1f1dac80092
Stefany Lopes <142550857+Stefanylopes28@users.noreply.github.com>
2024-11-08 18:58:56 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,87 @@
+// App.js
+import { useState } from "react";
+import * as S from "./style";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalculator } from "@fortawesome/free-solid-svg-icons";
+
+export default function App() {
+  const [primeiroValor, setPrimeiroValor] = useState(0);
+  const [segundoValor, setSegundoValor] = useState(0);
+  const [resultado, setResultado] = useState();
+
+  const capturandoPrimeiroValor = (e) => {
+    setPrimeiroValor(Number(e.target.value));
+  };
+
+  const capturandoSegundoValor = (e) => {
+    setSegundoValor(Number(e.target.value));
+  };
+
+  const soma = () => {
+    setResultado(primeiroValor + segundoValor);
+    limparCampos();
+  };
+
+  const subtracao = () => {
+    setResultado(primeiroValor - segundoValor);
+    limparCampos();
+  };
+
+  const multiplicacao = () => {
+    setResultado(primeiroValor * segundoValor);
+    limparCampos();
+  };
+
+  const divisao = () => {
+    if (segundoValor !== 0) {
+      setResultado(primeiroValor / segundoValor);
+    } else {
+      setResultado("Erro: Divisão por zero");
+    }
+    limparCampos();
+  };
+
+  const porcentagem = () => {
+    setResultado((primeiroValor * segundoValor) / 100);
+    limparCampos();
+  };
+
+  const limparCampos = () => {
+    setPrimeiroValor(0);
+    setSegundoValor(0);
+    document.getElementById("input1").value = "";
+    document.getElementById("input2").value = "";
+  };
+
+  return (
+    <S.MainContainer>
+      <S.GlobalStyle />
+      <S.CalculatorBox>
+        <S.Title>
+          Calculadora{" "}
+          <FontAwesomeIcon icon={faCalculator} style={{ marginLeft: "10px" }} />
+        </S.Title>
+        <S.Input1
+          id="input1"
+          type="number"
+          placeholder="Insira um número"
+          onChange={capturandoPrimeiroValor}
+        />
+        <S.Input2
+          id="input2"
+          type="number"
+          placeholder="Insira um número"
+          onChange={capturandoSegundoValor}
+        />
+        <S.ButtonContainer>
+          <S.Button onClick={soma}>+</S.Button>
+          <S.Button onClick={subtracao}>-</S.Button>
+          <S.Button onClick={multiplicacao}>*</S.Button>
+          <S.Button onClick={divisao}>/</S.Button>
+          <S.Button onClick={porcentagem}>%</S.Button>
+        </S.ButtonContainer>
+        <h3>{resultado}</h3>
+      </S.CalculatorBox>
+    </S.MainContainer>
+  );
+}
---
+++ src/style.js
@@ -0,0 +1,79 @@
+// style.js
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: pink;
+  }
+`;
+
+export const MainContainer = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding: 20px;
+`;
+
+export const CalculatorBox = styled.div`
+  background-color: #fff;
+  border-radius: 15px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 2em;
+  color: #51aff7;
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Input1 = styled.input`
+  width: 80%;
+  height: 40px;
+  margin-bottom: 20px;
+  padding: 5px;
+  font-size: 1em;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  text-align: center;
+`;
+
+export const Input2 = styled.input`
+  width: 80%;
+  height: 40px;
+  margin-bottom: 20px;
+  padding: 5px;
+  font-size: 1em;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  text-align: center;
+`;
+
+export const ButtonContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 10px;
+  width: 100%;
+`;
+
+export const Button = styled.button`
+  background-color: #51aff7;
+  color: pink;
+  border: none;
+  padding: 10px 0;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 5px;
+  &:hover {
+    color: white;
+  }
+`;
